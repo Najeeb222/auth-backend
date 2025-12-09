1bc29e27385f06b029ff897ddab188ccce79bc8c
Najeeb222 <najeeb13750@gmail.com>
2025-12-09 14:25:26 +0500

    feat: update CORS configuration to allow multiple origins

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: ["http://localhost:5173", "https://mern-stack-auth-drab.vercel.app"],
     credentials: true,
   })
 );
